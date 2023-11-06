e7c0f6a761ea085227e62de07471dab45249f8e4
Rayyan <rayyan.lodhi05@gmail.com>
2023-11-06 12:35:09 -0500

    first commit

---
+++ 69A.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+vectors = []
+for i in range(n):
+    vectors.append([int(x) for x in input().split()])
+    
+    # x = input().split()
+    # vectors.append(x)
+    
+# for j in range(n):
+#     vectors[j].split()
+print(vectors)
+
+
+
+'1 2 3'
+
